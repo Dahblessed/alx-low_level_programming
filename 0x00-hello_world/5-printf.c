c9b6a0a19b8fc45d9c0a37d1af941537426d6ff2
Barakat <barakatsalaudeen1@gmail.com>
2023-03-16 20:07:44 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  a C program that prints exact words
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
