d6b4c52e20a63bdf0bf091901fa1c1171f078914
Barakat <barakatsalaudeen1@gmail.com>
2023-03-16 21:43:10 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - a c programm that prints a line to he standard error.
+ * main - a c programm that prints a line to he standard error
+ *
  * Return:1 (successful)
  */
+
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
