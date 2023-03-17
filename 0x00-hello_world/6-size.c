24da7ae82f8ebfc06a24f305268157fceeff3cb3
Barakat <barakatsalaudeen1@gmail.com>
2023-03-16 22:34:43 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -  a C program that prints the size of various types on the computer
- * Return: 0 on success 
+ * main- a C program that prints the size of various types on the computer
+ * Return: 0 on success
  */
 int main(void)
 {
-
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
