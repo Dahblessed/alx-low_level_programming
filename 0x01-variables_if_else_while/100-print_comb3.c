172ed3957b564b1c3493c2d10864a98f53d30dfa
Barakat <barakatsalaudeen1@gmail.com>
2023-03-19 03:13:08 -0700

    Two digit combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,12 +18,13 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				if (i + j != 17)
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
